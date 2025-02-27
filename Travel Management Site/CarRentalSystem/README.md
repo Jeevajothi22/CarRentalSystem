77d71c6fdb72e6ac2acea990900b27dd98d042eb
Jeevajothi <119393224+Jeevajothi22@users.noreply.github.com>
2025-02-27 23:56:58 +0530

    Add files via upload

---
+++ Travel Management Site/CarRentalSystem/README.md
@@ -0,0 +1,5 @@
+A Travel Management Site for Cars is an online platform that simplifies car rental 
+and travel planning. It offers a variety of vehicles, flexible booking options, real-time 
+availability, and route planning tools. Designed for personal and corporate use, the 
+site enhances convenience, ensures transparent pricing, and provides a seamless 
+travel experience for users.
---
+++ Travel Management Site/includes/footer.php
@@ -0,0 +1,16 @@
+	<li class="about">
+					<ul>
+						<li><a href="http://facebook.com/projectworldsin" class="facebook" target="_blank"></a></li>
+						<li><a href="http://twitter.com/projectworlds32" class="twitter" target="_blank"></a></li>
+						<li><a href="http://plus.google.com/projectworlds32" class="google" target="_blank"></a></li>
+						<li><a href="#" class="skype"></a></li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+
+
+	</footer><!--  end footer  -->
+	
+</body>
+</html>
